344a17197c8ba7c753088395a4bffe8ab8730ff1
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-12 13:37:17 -0600

    tested sanity check, works!

--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
-const server = require('express')
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
 
 
+server.get('/', (req, res) => {
+    res.send('Sanity Check')
+})
 
 
 module.exports= server
--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server')
 const PORT = process.env.PORT || 5400
 
 server.listen(PORT, () => {
-    console.log(`Listening on port ${Port}...`)
+    console.log(`Listening on port ${PORT}...`)
 })
