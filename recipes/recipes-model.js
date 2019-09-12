79e85c610bf2b8f7c5fa265b0cd58075552ff475
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-12 14:18:55 -0600

    worked on recipes-model skeleton

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -1,1 +1,19 @@
 const db = require('../data/db-config')
+
+module.exports = {
+    getRecipes,
+    getShoppingList,
+    getInstructions
+}
+
+function getRecipes() {
+    //should return a list of all recipes in the database.
+}
+
+function getShoppingList(id) {
+    //should return a list of all ingredients and quantities for a given recipe
+}
+
+function getInstructions(id) {
+    //should return a list of step by step instructions for preparing a recipe
+}
