31e19ebe3789c0329812d9c328413a1fde548335
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-12 14:26:43 -0600

    worked on recipes and steps schema

--- data/migrations/20190912141313_relate-tables.js
+++ data/migrations/20190912141313_relate-tables.js
@@ -1,6 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('recipes', tbl => {
+        tbl.increments();
+        tbl.string('recipe_name', 255).unique().notNullable()
+    })
+    .createTable('steps', tbl => {
+        tbl.increments();
+        
+    })
+    .createTable('recipe_ingredients', tbl => {})
+    .createTable('ingredients', tbl => {})
 };
 
 exports.down = function(knex) {
