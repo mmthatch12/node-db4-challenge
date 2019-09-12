ec4acc29818d9c1442871cb6f900eb026117b9cf
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-12 14:11:24 -0600

    worked on recipes router and import router into server

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 
+const RecipesRouter = require('../recipes/recipes-router')
+
 const server = express()
 
 server.use(express.json())
+server.use('/recipes', RecipesRouter)
 
 
 server.get('/', (req, res) => {
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const Recipes = require('./recipes-model')
+
 const router = express.Router()
 
 module.exports = router
